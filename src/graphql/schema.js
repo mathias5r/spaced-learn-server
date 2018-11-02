5570ded1ea0275a7dddd0e2cc08aa64af8831588
Mathias5R <mathiassilva4@gmail.com>
2018-11-02 20:20:54 -0300

    Creating base graphql structure

---
+++ src/graphql/schema.js
@@ -0,0 +1,8 @@
+import path from 'path';
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+
+const types = fileLoader(path.join(__dirname, './types'));
+const declarations = fileLoader(path.join(__dirname, './declarations'));
+const typesDef =  mergeTypes([...types, ...declarations ], { all: true });
+
+export default typesDef;
