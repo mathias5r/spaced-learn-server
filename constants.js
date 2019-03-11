fc48fef03cb1cdb732cee5607e1a723aeea8a058
Mathias5R <mathiassilva4@gmail.com>
2019-03-10 23:34:55 -0300

    Creating Dockerfile and mongodb users entrypoint

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
-export const MONGO_DATABASE = "spacedlearn"
+export const MONGO_DATABASE = "spacedlearn";
 export const MONGO_USER = "root";
 export const MONGO_PASSWORD = "root";
 export const MONGO_SERVER = "localhost:27017/admin";
+export const JWT_SECRET_KEY = "sp4cedle4rnj5k";
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import { ApolloServer } from "apollo-server-express";
 import schema from "./graphql/schema";
 import resolvers from "./graphql/resolvers";
+import context from "./services/context";
 
 const server = new ApolloServer({
 	typeDefs: schema,
 	resolvers,
+	context,
 });
 
 export default server;
